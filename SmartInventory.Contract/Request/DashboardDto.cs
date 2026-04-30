9bf1e4fe0349b963d339277e3d0b63bde30515d2
muttakin43 <eamsiam6@gmail.com>
2026-04-30 12:36:16 +0600

    Dashboard init

---
+++ SmartInventory.Contract/Request/DashboardDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SmartInventory.Contract.Request
+{
+    public class DashboardDto
+    {
+        public int TotalProducts { get; set; }
+        public int TotalStock { get; set; }
+        public decimal TotalSales { get; set; }
+        public decimal TotalPurchase { get; set; }
+        public decimal Profit { get; set; }
+
+        // Chart
+        public List<string> Months { get; set; } = new();
+        public List<decimal> SalesData { get; set; } = new();
+
+        // Extra
+        public List<Product> LowStockProducts { get; set; } = new();
+        public List<StockTransaction> RecentTransactions { get; set; } = new();
+    }
+}
